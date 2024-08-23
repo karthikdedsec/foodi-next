471d47336816d5336eb884173eaa69e5a98d1b5d
karthik Raj <61104188+karthikdedsec@users.noreply.github.com>
2024-08-23 15:14:31 +0400

    add metadata

--- app/meals/page.js
+++ app/meals/page.js
@@ -4,6 +4,11 @@ import classes from "./page.module.css";
 import MealsGrid from "../components/meals/meals-grid";
 import { getMeals } from "@/lib/meals";
 
+export const metadata = {
+  title: "All Meals",
+  description: "Browse the delicious meals shared by our community",
+};
+
 const Meal = async () => {
   const meals = await getMeals();
   return <MealsGrid meals={meals} />;
